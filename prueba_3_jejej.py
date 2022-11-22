59f067b7d487a735601d033c3913cb500a8c9d0f
jhonatang73 <117460307+jhonatang73@users.noreply.github.com>
2022-11-21 19:18:53 -0500

    Update prueba_3_jejej.py

--- prueba_3_jejej.py
+++ prueba_3_jejej.py
@@ -77,3 +77,14 @@ import folium
 from folium.plugins import MiniMap
 mapa = folium.Map()
 mapa
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
